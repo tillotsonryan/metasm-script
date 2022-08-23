fdeb1325735eaa9e2bfad708680c0bebb7a5aafc
tillotsonryan <110366594+tillotsonryan@users.noreply.github.com>
2022-08-23 17:42:45 -0600

    Add files via upload

---
+++ metasm-script.rb
@@ -0,0 +1,12 @@
+require 'metasm'
+require 'rex'
+payload = <<-EOS
+	mov al, 0x1
+	int 0x80
+EOS
+
+sc = Metasm::Shellcode.assemble(Metasm::X86.new, payload).encode_string
+filename = "#{Rex::Text.rand_text_alpha_lower(8)}.c"
+f = File.new(filename,  "w")
+f.puts Rex::Text.to_c(sc)
+f.close
